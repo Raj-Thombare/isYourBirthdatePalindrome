caefe5128e71ba73a694224703dd9a580661240f
Raj Thombare (Rakaa) <86057670+Raj-Thombare@users.noreply.github.com>
2021-08-19 01:00:27 +0530

    refactor: leap year function

--- app.js
+++ app.js
@@ -67,18 +67,19 @@ function checkPalindromeForAllDateFormats(date) {
 }
 
 function leapYear(year) {
-    if (year % 400 === 0){
-        return true;
+    if(year%4===0){
+        if(year%100===0){
+            if(year%400===0){
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return true
+        }
+    }else{
+        return false
     }
-    if (year % 100 === 0){
-        return false;
-    }
-       
-    if (year % 4 === 0){
-        return true;
-    }
-        
-    return false;
 }
 
 function getNextDate(date) {
